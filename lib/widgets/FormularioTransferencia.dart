0196d93260f50cd8ed315c55c18913700c0fafc1
Cassio <cassioate@gmail.com>
2021-07-04 21:31:06 -0300

    Criando o layout do formulario

--- lib/widgets/FormularioTransferencia.dart
+++ lib/widgets/FormularioTransferencia.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
 class FormularioTransferencia extends StatelessWidget {
+  void onPressed() {
+    print('teste');
+  }
+
+  var child;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Criando Transferencia'),
       ),
-      body: Text('Teste'),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: "Número da conta",
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: TextField(
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: "Valor",
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          ElevatedButton(onPressed: onPressed, child: Text('Confirmar'))
+        ],
+      ),
     );
     ;
   }
