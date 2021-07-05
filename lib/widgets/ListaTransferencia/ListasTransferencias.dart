3e6e4eda998a979a38dae3422b368a1131ab3f27
Cassio <cassioate@gmail.com>
2021-07-05 01:05:33 -0300

    Utilizanod ListView para ItemTransferencia em ListasTrasnferencias (OBS: falta mudar de statelesse para stateful o Widget, para que assim a transferencia seja feita via tela e não igual foi realizado na linha 23

--- lib/widgets/ListaTransferencia/ListasTransferencias.dart
+++ lib/widgets/ListaTransferencia/ListasTransferencias.dart
@@ -5,25 +5,32 @@ import 'package:projeto1/widgets/FormularioTransferencia/FormularioTransferencia
 import 'package:projeto1/widgets/ItemTransferencia/ItemTransferencia.dart';
 
 class ListasTransferencias extends StatelessWidget {
+  List<Transferencia?> _transferencias = [];
+
   _mudarPagina(context) {
     Future<Transferencia?> future = Navigator.push(context,
         MaterialPageRoute(builder: (context) => FormularioTransferencia()));
     debugPrint("Listas transferencias");
-    future.then((value) =>
-        {debugPrint("Chegou no then do FUTURE"), debugPrint("$value")});
+    future.then((value) => {
+          debugPrint("Chegou no then do FUTURE"),
+          debugPrint("$value"),
+          if (value != null) {_transferencias.add(value)}
+        });
   }
 
   @override
   Widget build(BuildContext context) {
+    _transferencias.add(Transferencia(300, 200));
     return Scaffold(
       appBar: AppBar(
         title: Text('Transferencias'),
       ),
-      body: Column(
-        children: [
-          ItemTransferencia(Transferencia(300, 3000)),
-          ItemTransferencia(Transferencia(1000, 5000)),
-        ],
+      body: ListView.builder(
+        itemCount: _transferencias.length,
+        itemBuilder: (context, indice) {
+          Transferencia _transferencia = _transferencias[indice]!;
+          return ItemTransferencia(_transferencia);
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _mudarPagina(context),
